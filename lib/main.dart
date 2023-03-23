cbf757d667ba0ce07bc2f2e6a4b400b412dcf1b4
Abdul <abdul.tanveer@gmail.com>
2023-03-23 12:32:20 +0530

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iiflb1/networking/album.dart';
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(MaterialApp(
@@ -28,5 +34,22 @@ void main() {
   ));
 }
 
+
+
 _increment() {
+}
+
+Future<Album> fetchAlbum(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
--- lib/networking/myapp.dart
+++ lib/networking/myapp.dart
@@ -23,6 +23,8 @@ Future<Album> fetchAlbum() async {
 
 
 
+
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
